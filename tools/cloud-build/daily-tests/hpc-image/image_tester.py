d67f7897260b2fb5499b09a59742006c83975273
Carson Dunbar <carsondunbar@google.com>
2024-01-24 14:20:30 +0000

    Saving progress, squash in future

--- tools/cloud-build/daily-tests/hpc-image/image_tester.py
+++ tools/cloud-build/daily-tests/hpc-image/image_tester.py
@@ -19,17 +19,19 @@ import tarfile
 import argparse
 import shutil
 import subprocess
+from typing import List
 
 # TODO Replace gcloud commands with gcloud pip module commands
 ROOT_DIR = sys.path[0]
 BUILD_PROJECT_ID = "cloud-hpc-image-devel"
 GHPC_DIR = os.path.join(ROOT_DIR, "hpc-toolkit")
 GHPC_BIN_SRC = os.path.join(GHPC_DIR, "ghpc")
-GHPC_BIN_DST = os.path.join(ROOT_DIR, "ghpc")
+GHPC_BIN_DST = os.path.join("/usr/local/bin", "ghpc")
 NET_NAME = "hpc-image-testing-net"
 NET_BP = os.path.join(ROOT_DIR, "testing-vpc.yaml")
 TEMPLATE_BP = os.path.join(ROOT_DIR, "intel-mpi-bm-template.yaml")
 NET_DEP_FOLDER = "hpc-image-vpc"
+DEPLOYMENT_BUCKET="hpc-image-deployments"
 ENV_COPY = os.environ.copy()
 
 DESCRIPTION = '''
@@ -51,7 +53,7 @@ def run_command(cmd: str, err_msg: str = None) -> subprocess.CompletedProcess:
     return res
 
 def install_go(go_ver: str = "1.21.5") -> None:
-    if os.path.exists("go"):
+    if shutil.which("go") is not None:
         return
     print("Golang not found on system, attempting to install precompiled package")
     fn = f"go{go_ver}.linux-amd64.tar.gz"
@@ -79,7 +81,7 @@ def get_latest_n_images(project: str, img_family: str, n: int = 3) -> None:
                   f"{img_family}")
         return imgs
     print(f"No images were found for the {img_family} family, exiting")
-    sys.exit()
+    sys.exit(1)
 
 def get_data(image_name: str, deployment_name: str, output_dir: str) -> None:
     cmd = "gcloud compute scp --recurse --project {project} --zone {zone} " \
@@ -87,20 +89,20 @@ def get_data(image_name: str, deployment_name: str, output_dir: str) -> None:
           f"{output_dir}/{image_name} </dev/null"
     run_command(cmd, "Error downloading Ramble test results")
 
+def send_deployment_to_bucket(deployment_name: str, bucket_name: str) -> None:
+    outfile = f"{deployment_name}.tar.gz"
+    with tarfile.open(outfile, "w:gz") as tar:
+        tar.add(deployment_name, arcname=os.path.basename(deployment_name))
+    cmd = f"gsutil cp {outfile} gs://{bucket_name}"
+    run_command(cmd, "Error downloading Ramble test results")
+
 def build_ghpc(branch: str = "develop") -> None:
-    if os.path.exists(GHPC_BIN_DST):
+    if shutil.which("ghpc") is not None:
         print("GHPC already in the correct location, not rebuilding")
         return
     if os.path.exists(GHPC_DIR):
         shutil.rmtree(GHPC_DIR)
     # install_go()
-    print("Cloning HPC Toolkit")
-    cmd = f"git clone https://github.com/GoogleCloudPlatform/hpc-toolkit.git {GHPC_DIR}"
-    run_command(cmd, "Error cloning HPC Toolkit repo")
-    cmd = f"git checkout origin/{branch}"
-    os.chdir(GHPC_DIR)
-    print(f"Checking out {branch} branch")
-    run_command(cmd, f"Error checking out to {branch} branch")
     print("Building ghpc")
     run_command("make", "Error making ghpc")
     shutil.copy(GHPC_BIN_SRC, GHPC_BIN_DST)
@@ -112,9 +114,9 @@ def build_network() -> None:
           f"--format='value(name)' --filter='name:{NET_NAME}'"
     res = run_command(cmd, "Error getting list of gcloud networks")
     if res.stdout is None:
-        cmd = f"./ghpc create -w --vars project_id={BUILD_PROJECT_ID} {NET_BP}"
+        cmd = f"ghpc create -w --vars project_id={BUILD_PROJECT_ID} {NET_BP}"
         run_command(cmd, "Error creating network deployment folder")
-        cmd = f"./ghpc deploy {NET_DEP_FOLDER} --auto-approve"
+        cmd = f"ghpc deploy {NET_DEP_FOLDER} --auto-approve"
         run_command(cmd, "Error deploying network")
 
 def create_deployment(project:str, image_name: str, zone: str,
@@ -124,7 +126,7 @@ def create_deployment(project:str, image_name: str, zone: str,
         build_id = os.environ["BUILD_ID"][:6]
     else:
         build_id = uuid.uuid4().hex[:6]
-    dep_name=f"test-{image_name}-{build_id}"
+    dep_name=f"{image_name}-{build_id}"
     dep_vars = [f"ramble_config_location={ramble_file}",
                 "add_deployment_name_before_prefix=true",
                 f"compute_machine_type={machine_type}",
@@ -133,25 +135,32 @@ def create_deployment(project:str, image_name: str, zone: str,
                 f"region={zone[:-2]}",
                 f"zone={zone}",
                 f"deployment_name={dep_name}"]
-    cmd = f"./ghpc create -w --vars project_id={BUILD_PROJECT_ID} " \
+    cmd = f"ghpc create -w --vars project_id={BUILD_PROJECT_ID} " \
           f"--vars {','.join(dep_vars)} {TEMPLATE_BP}"
     run_command(cmd, "Error creating microbenchmark deployment")
+    send_deployment_to_bucket(dep_name, DEPLOYMENT_BUCKET)
     return dep_name
 
-def deploy_tests(dep_dir: str) -> None:
-    cmd = f"./ghpc deploy {dep_dir} --auto-approve"
+def deploy_tests(dep_dir: str) -> str:
+    cmd = f"ghpc deploy {dep_dir} --auto-approve"
     try:
         run_command(cmd, "Error deploying microbenchmark tests")
+        send_deployment_to_bucket(dep_dir, DEPLOYMENT_BUCKET)
     except subprocess.SubprocessError as e:
         print(f"{e}\nDestroying deployment then exiting")
         destroy_deployment(dep_dir)
-        sys.exit()
+        if "does not have enough resources" in e.__str__():
+            return "stockout"
+        else:
+            sys.exit(1)
+    return ""
 
 def destroy_deployment(dep_dir: str) -> None:
-    cmd = f"./ghpc destroy {dep_dir} --auto-approve"
+    cmd = f"ghpc destroy {dep_dir} --auto-approve"
     run_command(cmd, "Error destroying microbenchmark deployment")
+    send_deployment_to_bucket(dep_dir, DEPLOYMENT_BUCKET)
 
-def run_tests(project: str, zone: str, ramble_file: str,
+def run_tests(project: str, ramble_file: str, zones: List[str] = ["us-central1-a"],
               img_family: str = None, img_names: str = None, n: int = 3,
               machine_type: str = "c2-standard-60"):
     if machine_type is None:
@@ -168,18 +177,22 @@ def run_tests(project: str, zone: str, ramble_file: str,
     build_ghpc()
     build_network()
     img_names.update(get_latest_n_images(project, img_family, n))
+    zones = zones.split(",")
     print("Testing the following images:")
     for img in img_names:
         print(img)
     for img in img_names:
         print(f"Starting tests on image: {img}")
-        print("Creating deployment")
-        rel_dep_dir = create_deployment(project, img, zone, ramble_file, machine_type)
-        dep_dir = os.path.join(ROOT_DIR, rel_dep_dir)
-        print("Deploying tests")
-        deploy_tests(dep_dir)
-        print("Destroying tests")
-        destroy_deployment(dep_dir)
+        for zone in zones:
+            print(f"Creating deployment in zone: {zone}")
+            rel_dep_dir = create_deployment(project, img, zone, ramble_file, machine_type)
+            dep_dir = os.path.join(ROOT_DIR, rel_dep_dir)
+            print("Deploying tests")
+            res = deploy_tests(dep_dir)
+            if res == "":
+                print("Tests succeeded, destroying deployment")
+                destroy_deployment(dep_dir)
+                break
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='hpc-image-tester.py',
@@ -199,8 +212,11 @@ if __name__ == "__main__":
     parser.add_argument("-i", "--image_names",
                         help="Comma delimited list of images to test from " \
                              "project")
-    parser.add_argument("-z", "--zone",
-                        help="Zone to run test in (default = us-central1-a)")
+    parser.add_argument("-z", "--zones",
+                        help="Comma delimited list of zones to run test in " \
+                             "(default = us-central1-a). They are tried in " \
+                             "order until one runs to completion or the list" \
+                             " is exhausted")
 
     if len(sys.argv)==1:
         parser.print_help(sys.stderr)
@@ -213,5 +229,5 @@ if __name__ == "__main__":
 
     rf = os.path.abspath(args.ramble_file)
 
-    run_tests(args.project, args.zone, rf, args.image_family,
+    run_tests(args.project, rf, args.zones, args.image_family,
               args.image_names, args.num_images, args.machine_type)
